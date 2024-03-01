3cf89fe2520f26cc551a3a59691aec00ffde6cfb
nareshkrishnaa <nareshkrishnaaa@gmail.com>
2024-03-01 09:53:35 +0530

    feature : check box added to the seat selector component, need to make it better looking

--- src/components/SeatSelector.js
+++ src/components/SeatSelector.js
@@ -1,13 +1,55 @@
 import React from "react";
 import { Container, Form } from "react-bootstrap";
+import "./SeatSelector.scss";
 
 const SeatSelector = () => {
   let totalSeats = 50;
   let availableSeats = 20;
+  let busRow = 10;
+  let busCol = 5;
+  let counter = 0;
+
   return (
     <div>
       <Container>
-        <Form>
+        {[...Array(busRow).keys()].map((row) => {
+          return (
+            <Form className="border">
+              {[...Array(busCol).keys()].map((col) => {
+                counter++;
+                return (
+                  <Form.Check
+                    key={`${row}-${col}`}
+                    inline
+                    label={`${counter}`}
+                    type="checkbox"
+                    className={col === 1 ? "red-box" : ""}
+                  />
+                );
+              })}
+            </Form>
+          );
+        })}
+
+        {/* {[...Array(busRow).keys()].map((row) => {
+          return (
+            <Form className="border p-5">
+              {`Row ${row + 1}`}
+              {[...Array(busCol).keys()].map((col) => {
+                counter++;
+                return <span key={col} className="px-5">{` ${counter}`}</span>;
+              })}
+            </Form>
+          );
+        })} */}
+
+        {/* {[...Array(busRow).keys()].map((row) =>
+          [...Array(busCol).keys()].map((col) => {
+            return <div className="border p-5"></div>;
+          }),
+        )} */}
+
+        {/* <Form>
           {[...Array(10).keys()].map((row, indexRow) =>
             [...Array(3).keys()].map((col, indexCol) => {
               console.log(indexRow + 1, indexCol + 1);
@@ -20,7 +62,7 @@ const SeatSelector = () => {
               );
             }),
           )}
-        </Form>
+        </Form> */}
       </Container>
     </div>
   );
